066ed32e7ce45666d6083a1ffd3aea4c6128efb8
Rivest Osz <rivest-oss@proton.me>
2024-03-23 01:12:35 -0300

    Changed let->const, removed setInterval

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let params = {};
+const params = {};
 window.location.search.slice(1).split(/&/g).forEach(x => {
-	let y = x.split(/=/g);
-	params[y[0]] = decodeURIComponent(y[1]);
+	params[y[0]] = decodeURIComponent(x.split(/=/g)[1]);
 });
 
-let retryNode = document.createElement(`span`);
-retryNode.classList.add(`top-left`);
-retryNode.innerText = `🔄`;
-retryNode.onclick = function() {
+const retryNode = document.createElement("span");
+retryNode.classList.add("top-left");
+retryNode.innerText = "🔄";
+retryNode.onclick = () => {
 	window.location.replace(params.url);
 };
 
 document.title = `HTTP.cat - ${params.statusCode}`;
 
-let divNode = document.createElement(`div`);
-divNode.classList.add(`center`);
+const divNode = document.createElement("div");
+divNode.classList.add("center");
 
-let imgNode = document.createElement(`img`);
+const imgNode = document.createElement("img");
 imgNode.src = `./imgs/${params.statusCode}.jpg`;
 //imgNode.src = `https://http.cat/images/${params.statusCode}.jpg`;
 
@@ -24,11 +23,11 @@ document.body.appendChild(divNode);
 divNode.appendChild(imgNode);
 document.body.appendChild(retryNode);
 
-let audio = new Audio(`./05. Eggy Toast - Saxophone guy.mp3`);
+const audio = new Audio("./05. Eggy Toast - Saxophone guy.mp3");
 audio.loop = true;
 
 function checkPause() {
-	if(window.localStorage.pause == `1`) {
+	if(window.localStorage.pause === "1") {
 		audio.pause();
 	} else {
 		audio.play();
@@ -37,11 +36,9 @@ function checkPause() {
 
 checkPause();
 
-document.onkeypress = (ev) => {
-	if(ev.code == `Space`) {
-		window.localStorage.pause = window.localStorage.pause == `1` ? `0` : `1`;
+document.onkeypress = ev => {
+	if(ev.code == "Space") {
+		window.localStorage.pause = window.localStorage.pause === "1" ? "0" : "1";
 		checkPause();
 	}
 };
-
-setInterval(checkPause, 700);
